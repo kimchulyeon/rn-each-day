c3633eb0e8c2cba7d969d218f0e6e9f9e3ccbbf8
chulyeon <guinness987@gmail.com>
2024-10-30 22:06:13 +0900

    feat: BottomTabItem 아이콘 적용 및 패키지 추가

--- src/navigations/tab/HomeTabNavigator.tsx
+++ src/navigations/tab/HomeTabNavigator.tsx
@@ -3,12 +3,24 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import FeedStackNavigator from '../stack/FeedStackNavigator';
 import AddFeedNavigator from '../stack/AddFeedStackNavigator';
 import ProfileStackNavigator from '../stack/ProfileStackNavigator';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {Brown} from '@/constants';
 
 export type HomeTabParamList = {
   Feed: undefined;
   Add_Post: undefined;
   Profile: undefined;
 };
+
+const TabBarIcon = ({focused, name}: {focused: boolean; name: string}) => (
+  <>
+    {focused ? (
+      <Icon name={name} size={30} color={Brown.Primary} />
+    ) : (
+      <Icon name={`${name}-outline`} size={30} color={Brown.Secondary} />
+    )}
+  </>
+);
 
 export default function HomeTabNavigator() {
   const Tab = createBottomTabNavigator<HomeTabParamList>();
@@ -18,9 +30,30 @@ export default function HomeTabNavigator() {
       screenOptions={{
         headerShown: false,
       }}>
-      <Tab.Screen name="Feed" component={FeedStackNavigator} />
-      <Tab.Screen name="Add_Post" component={AddFeedNavigator} />
-      <Tab.Screen name="Profile" component={ProfileStackNavigator} />
+      <Tab.Screen
+        name="Feed"
+        component={FeedStackNavigator}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({focused}) => TabBarIcon({focused, name: 'book'}),
+        }}
+      />
+      <Tab.Screen
+        name="Add_Post"
+        component={AddFeedNavigator}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({focused}) => TabBarIcon({focused, name: 'add-circle'}),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileStackNavigator}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({focused}) => TabBarIcon({focused, name: 'person'}),
+        }}
+      />
     </Tab.Navigator>
   );
 }
